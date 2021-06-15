3481aec57824e73da32dd7b2c35599c16895aa98
anamikasawhney <anamika.sawhney22@gmail.com>
2021-06-15 12:27:53 +0530

    Entity Framework & Layots Demo Project

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication15.Models;
+
+namespace WebApplication15.Controllers
+{
+    public class CourseController : Controller
+    {
+        CourseDbContext db = new CourseDbContext();
+        // GET: Course
+        public ActionResult Index()
+        {
+            return View(db.courses.ToList());
+        }
+
+
+        public ActionResult Details(int? id)
+        {
+            if(id.HasValue)
+            {
+                Course course = db.courses.FirstOrDefault(x => x.Id == id);
+                if (course == null)
+                    return View(course);
+                else
+                {
+                    ViewBag.msg = "Course with this ID not found";
+                    return View();
+                }
+            }
+            else
+            {
+                ViewBag.msg = "Please Provide ID";
+                return View();
+            }
+        }
+
+        public ActionResult Create()
+        {
+            Course course = new Course();
+            return View(course);
+        }
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            db.courses.Add(course);
+            db.SaveChanges();
+            TempData["msg"] ="Record inserted";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/CourseDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication15.Models
+{
+    public class CourseDbContext : DbContext
+    {
+        public DbSet<Course> courses { get; set; }
+    }
+}
